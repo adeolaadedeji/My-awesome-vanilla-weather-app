e82eaa0d58db6f7659178ebb438787a48449fbc6
adeolaadedeji <delaji@yahoo.com>
2022-02-10 15:22:32 +0100

    Updated the JS file

--- app.js
+++ app.js
@@ -69,7 +69,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "73b32b3410f09080307c21fa3b1e4394";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
